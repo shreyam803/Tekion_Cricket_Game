8c923478f6144e0158ae2df8d60b0563eeeebdf1
Shreya Maheshwari <shreyam803@gmail.com>
2023-03-02 12:49:33 +0530

    changed play nethod

--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -5,13 +5,13 @@ public class Scoreboard {
         System.out.println("Score of "+ team2.getName()+" is " + team2.getScore() + " with " + team2.getWickets() + " wickets.");
 
         if(team1.getScore() > team2.getScore()){
-            System.out.println(team1.getName() + " won the match by " + (10-team1.getWickets()) + " wickets.");
+            System.out.println(team1.getName() + " won the match by " + team1.getScore() + " runs.");
         }
         else if(team1.getScore() == team2.getScore()){
             System.out.println("Equal Scores!! Match Tie.");
         }
         else {
-            System.out.println(team2.getName()+ " won the match by " + (10-team2.getWickets()) + " wickets.");
+            System.out.println(team2.getName()+ " won the match by " + team2.getScore() + " runs.");
         }
     }
 }
